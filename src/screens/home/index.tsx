06e78d00fbb9b58cf288f19274d801ec8588e0d3
samir.taghizada1 <samir.taghizada@go-e.com>
2025-05-08 21:36:56 +0400

    Add memo

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,7 +1,35 @@
+import {useState} from "react"
+
 export const HomeScreen = () => {
+  const [isAdmin, setIsAdmin] = useState(false)
+
   return (
     <div>
       <h4>Home screen</h4>
+      <Counter />
+      <p>User type: {isAdmin ? 'admin' : 'regular'}</p>
+      <button onClick={() => setIsAdmin(prev => !prev)}>Change user type</button>
+      <ChildComponent />
+    </div>
+  )
+}
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0)
+  return (
+
+    <>
+      <p>Counter: {counter}</p>
+      <button onClick={() => setCounter(prev => prev + 1)}>Increment</button>
+    </>
+  )
+}
+
+const ChildComponent = () => {
+  console.log('RERENDER CHILD COMPONENT')
+  return (
+    <div>
+      <p>Child component</p>
     </div>
   )
 }
